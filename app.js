3e819a2baab42575ba5ef3b20529b8705ac5db2a
Saman <samanzahid012@gmail.com>
2022-08-18 17:05:16 +0500

    adding grade

--- app.js
+++ app.js
@@ -12,4 +12,23 @@ function calculate(){
 
     result = (english + maths + urdu + computer + accounting + marketing + islamiyat + java + it + ai) * 100/ 1000;
     document.querySelector("#result").innerHTML = `You have Got ${result}% Marks`;
-}
+
+
+     document.querySelector("#result").value;
+    if( result >= 80){
+       document.querySelector("#grade").innerHTML= "<h1 style='color:green;'>Congratulations!</h1> A1 Grade";
+    }
+    else if (result >= 70 && result < 80){
+        document.querySelector("#grade").innerHTML= "<h1 style='color:blue;'>Congratulations!</h1> A Grade";
+    }
+    else if (result >= 60 && result < 70){
+        document.querySelector("#grade").innerHTML= "<h1 style='color:pink;'>Congratulations!</h1> B Grade";
+    } 
+    else if (result >= 50 && result < 60){
+        document.querySelector("#grade").innerHTML= "<h1 style='color:yellow;'>Congratulations!</h1> C Grade";
+    }
+    else {
+        document.querySelector("#grade").innerHTML= "<h1 style='color:red;'></h1> Fail";
+    }
+    
+} 
